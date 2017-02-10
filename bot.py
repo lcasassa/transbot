2c0c7bcee6672ef962b9ab50d048cc6913c7e4a2
Linus Casassa <lcasassa@gmail.com>
2017-02-10 06:46:27 -0300

    Reordering imports.

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
-from telegram.ext import Updater
+import logging
+from telegram.ext import Updater, CommandHandler
 
 updater = Updater(token='361644162:AAGbFjy05HbhgK5CptAp6RNPuj70-AjmU8E')
 dispatcher = updater.dispatcher
 
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
-from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
