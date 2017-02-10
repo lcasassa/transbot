8f66a0fe4a1b1fa5876b834bdb96c0f59563a785
Linus Casassa <lcasassa@gmail.com>
2017-02-10 06:33:07 -0300

    implementing /start command.

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print "Hola mundo"
+from telegram.ext import Updater
+
+updater = Updater(token='361644162:AAGbFjy05HbhgK5CptAp6RNPuj70-AjmU8E')
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.idle()
